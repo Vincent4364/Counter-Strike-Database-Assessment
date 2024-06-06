ee295cc5ef59dd94abf8dd0bb932e205005df509
vincent.gregorashgmail.com <21060@wc.school.nz>
2024-06-06 14:16:22 +1200

    Created Database + began working on display and base html files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/base.html')
+def render_webpage():  # put application's code here
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
