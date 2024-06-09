f9bc1a94d0526af9afb9d38eb0a78ac4020eede9
vincent.gregorashgmail.com <21060@wc.school.nz>
2024-06-10 09:45:15 +1200

    Experimented with directorys and the disply of data

--- app.py
+++ app.py
@@ -3,17 +3,63 @@ import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "CS_DATA.db"
+
+
+def create_connection(db_filename):
+    try:
+        connection = sqlite3.connect(db_filename)
+        return connection
+    except Error as e:
+        print(e)
+        return None
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home_page():
+    return render_template('index.html')
 
 
-@app.route('/base.html')
-def render_webpage():  # put application's code here
-    return render_template('base.html')
+@app.route('/display/<table_type>')
+def render_webpages_page(table_type):
+    query = """
+    SELECT rating, headshot_percentage, kd_ratio, teams_played_in, country, player_name, total_kills
+    FROM player_stats
+    WHERE type = ?
+    """
+    connection = create_connection(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(query, (table_type,))
+    data_list = cursor.fetchall()
+    connection.close()
+
+    return render_template('display.html', data=data_list, table_type=table_type)
+
+@app.route('/search', methods=['GET', 'POST'])
+def render_search_page():
+    if request.method == 'POST':
+        look_up = request.form['Search']
+        search_term = look_up
+        title = "Search for: '" + look_up + "'"
+        look_up = "%" + look_up + "%"
+
+        query = """
+        SELECT rating, headshot_percentage, kd_ratio, teams_played_in, country, player_name, total_kills
+        FROM player_stats
+        WHERE player_name LIKE ? OR teams_played_in LIKE ? OR country LIKE ?
+        """
+        connection = create_connection(DATABASE)
+        cursor = connection.cursor()
+        cursor.execute(query, (look_up, look_up, look_up))
+        data_list = cursor.fetchall()
+        connection.close()
+
+        no_results = len(data_list) == 0
+
+        return render_template('display.html', data=data_list, search_term=search_term, no_results=no_results)
+
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
